d11071071717091b6a0187df2521ec4e635de792
hsachie <sachie.duck.1113@icloud.com>
2026-08-30 23:36:10 +0900

    end

---
+++ ex3/ft_custom_errors.py
@@ -0,0 +1,39 @@
+class GardenError (Exception):
+	def __init__(self, message = "Unknown garden error"):
+		super().__init__(message)
+
+class PlantError (GardenError):
+	def __init__(self, message = "Unknown plant error"):
+		super().__init__(message)
+
+class WaterError (GardenError):
+	def __init__(self, message = "Unknown water error"):
+		super().__init__(message)
+
+def gardenerror_test():
+	print("=== Custom Garden Errors Demo ===")
+	print("Testing PlantError...")
+	try:
+		raise PlantError("The tomato plant is wilting!")
+	except PlantError as err:
+		print("Caught PlantError:", err)
+	print("Testing WaterError...")
+	try:
+		raise WaterError("Not enough water in the tank!")
+	except WaterError as err:
+		print("Caught WaterError:", err)
+
+	print("Testing catching all garden errors...")
+	try:
+		raise PlantError("The tomato plant is wilting!")
+	except GardenError as err:
+		print("Caught GardenError: ", err)
+	try:
+		raise WaterError("Not enough water in the tank!")
+	except GardenError as err:
+		print("Caught GardenError: ", err)
+
+	print("All custom error types work correctly!")
+
+if __name__ == "__main__":
+	gardenerror_test()
---
+++ ex4/ ft_finally_block.py
@@ -0,0 +1,43 @@
+class GardenError (Exception):
+	def __init__(self, message = "Unknown garden error"):
+		super().__init__(message)
+
+class PlantError (GardenError):
+	def __init__(self, message = "Unknown plant error"):
+		super().__init__(message)
+
+def water_plant(plant_name: str) -> None:
+	if plant_name == plant_name.capitalize():
+		print(f"Watering {plant_name}: [OK]")
+	else:
+		raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+
+def test_watering_system() -> None:
+	print("=== Garden Watering System ===")
+	print("Testing valid plants...")
+	print("Opening watering system")
+	try:
+		water_plant("Tomato")
+		water_plant("Lettuce")
+		water_plant("Carrots")
+	finally:
+		print("Closing watering system")
+
+	print("Testing invalid plants...")
+	print("Opening watering system")
+	try:
+		water_plant("Tomato")
+		water_plant("lettuce")
+		water_plant("Carrots")
+	except PlantError as err:
+		print(f"Caught PlantError: {err}")
+		print(".. ending tests and returning to main")
+		return
+	finally:
+		print("Closing watering system")
+		print("Cleanup always happens, even with errors!")
+
+
+
+if __name__ == "__main__":
+	test_watering_system()
